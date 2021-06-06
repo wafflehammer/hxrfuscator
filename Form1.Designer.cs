a5e4518a2c10d3bd172021e9049469ec34a8df38
wafflehammer <neitys@gmail.com>
2021-06-06 12:34:11 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace nigfuscator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectProjectButton = new System.Windows.Forms.Button();
+            this.SelectOutputFolderButton = new System.Windows.Forms.Button();
+            this.NiggersLolButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectProjectButton
+            // 
+            this.SelectProjectButton.Location = new System.Drawing.Point(47, 163);
+            this.SelectProjectButton.Name = "SelectProjectButton";
+            this.SelectProjectButton.Size = new System.Drawing.Size(182, 47);
+            this.SelectProjectButton.TabIndex = 0;
+            this.SelectProjectButton.Text = "Select Project Directory";
+            this.SelectProjectButton.UseVisualStyleBackColor = true;
+            this.SelectProjectButton.Click += new System.EventHandler(this.SelectProjectButton_Click);
+            // 
+            // SelectOutputFolderButton
+            // 
+            this.SelectOutputFolderButton.Enabled = false;
+            this.SelectOutputFolderButton.Location = new System.Drawing.Point(572, 163);
+            this.SelectOutputFolderButton.Name = "SelectOutputFolderButton";
+            this.SelectOutputFolderButton.Size = new System.Drawing.Size(182, 47);
+            this.SelectOutputFolderButton.TabIndex = 1;
+            this.SelectOutputFolderButton.Text = "Select Output Directory";
+            this.SelectOutputFolderButton.UseVisualStyleBackColor = true;
+            this.SelectOutputFolderButton.Click += new System.EventHandler(this.SelectOutputFolderButton_Click);
+            // 
+            // NiggersLolButton
+            // 
+            this.NiggersLolButton.Enabled = false;
+            this.NiggersLolButton.Location = new System.Drawing.Point(292, 391);
+            this.NiggersLolButton.Name = "NiggersLolButton";
+            this.NiggersLolButton.Size = new System.Drawing.Size(218, 47);
+            this.NiggersLolButton.TabIndex = 2;
+            this.NiggersLolButton.Text = "Engage \'Hard-R\' Niggafuscation";
+            this.NiggersLolButton.UseVisualStyleBackColor = true;
+            this.NiggersLolButton.Click += new System.EventHandler(this.NiggersLolButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::nigfuscator.Properties.Resources.symbol_black_power_1_orig;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NiggersLolButton);
+            this.Controls.Add(this.SelectOutputFolderButton);
+            this.Controls.Add(this.SelectProjectButton);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectProjectButton;
+        private System.Windows.Forms.Button SelectOutputFolderButton;
+        private System.Windows.Forms.Button NiggersLolButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using Mono.Cecil;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace nigfuscator
+{
+    public partial class Form1 : Form
+    {
+
+        String asmFile = string.Empty;
+        String justName = "niggerslol.dll";
+        String outputFolder = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectProjectButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialogue = new OpenFileDialog())
+            {
+             
+                if (openFileDialogue.ShowDialog() == DialogResult.OK)
+                {
+                    asmFile = openFileDialogue.FileName;
+                    SelectOutputFolderButton.Enabled = true;
+                }
+            }
+
+        }
+
+        private void SelectOutputFolderButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog openFolderDialogue = new FolderBrowserDialog())
+            {
+
+                if (openFolderDialogue.ShowDialog() == DialogResult.OK)
+                {
+                    outputFolder = openFolderDialogue.SelectedPath;
+                    SelectOutputFolderButton.Enabled = true;
+                }
+            }
+            NiggersLolButton.Enabled = true;
+        }
+
+        private void NiggersLolButton_Click(object sender, EventArgs e)
+        {
+            AssemblyDefinition asm = AssemblyDefinition.ReadAssembly(asmFile);
+            var numberOfNiggers = 0;
+            foreach (TypeDefinition t in asm.MainModule.Types)
+            {
+                if (!(t.Name == "<Module>"))//global type
+                {
+                    foreach (MethodDefinition m in t.Methods)
+                    {
+                        Console.WriteLine(m.Name);
+                        m.Name = "NiggersLol" + numberOfNiggers;
+                        numberOfNiggers++;
+                       
+                        // You better use a random name & an array where you can put
+                        // names that has been already used in order to avoid having twice
+                        // the same name
+                    }
+                }
+            }
+         //   asm.MainModule.Write(Path.Combine(outputFolder, justName));
+
+        }
+    }
+}
